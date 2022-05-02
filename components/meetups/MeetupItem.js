fcab165488f3b90b205c8619ecacf0de77b3010e
Sevda Shukrieva <sevda.shukrieva@xdesign.com>
2022-05-02 22:32:01 +0100

    add meetup component for items

---
+++ components/meetups/MeetupItem.js
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import Card from '../ui/Card';
+import classes from './MeetupItem.module.css';
+
+function MeetupItem({ image, title, address }) {
+    return (
+        <li className={classes.item}>
+            <Card>
+                <div className={classes.image}>
+                    <Image src={image} alt={title} />
+                </div>
+                <div className={classes.content}>
+                    <h3>{title}</h3>
+                    <address>{address}</address>
+                </div>
+                <div className={classes.actions}>
+                    <button>Show Details</button>
+                </div>
+            </Card>
+        </li>
+    );
+}
+
+export default MeetupItem;
