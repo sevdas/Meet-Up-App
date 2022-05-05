189596768e26de4c676ad2ae1345fd47ff82ea9a
Sevda Shukrieva <sevda.shukrieva@xdesign.com>
2022-05-05 22:05:52 +0100

    add API router

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,6 +1,11 @@
 // api/new-meetup
 
 export default function handler(req, res) {
-    console.log('req', req)
+    console.log('req', req.method)
+    console.log('body', req.body)
+    if (req.method === 'POST') {
+        const data = req.body
+        const { title, image, address, description } = data
+    }
 }
 
